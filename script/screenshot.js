6379ec24342596641b32ac5201d36e38abf02c60
baezbenjamin <baezbenjamin03@gmail.com>
2025-12-04 23:12:57 -0300

    Address Fixed

--- script/screenshot.js
+++ script/screenshot.js
@@ -1,7 +1,7 @@
 const ssButton = document.querySelector("a");
 
-const siteAddress = new URLSearchParams(window.location.search);
-
+const siteAddress = window.location.href;
+console.log(siteAddress);
 let takeImage = `https://shot.screenshotapi.net/v3/screenshot?token=RA18FXX-4804ZR6-NZRC1SC-MAP6P5R&fresh=true&url=${siteAddress}&output=image&file_type=png&wait_for_event=load`;
 
 ssButton.setAttribute("href", takeImage);
