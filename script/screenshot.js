95aa12f3bed9893be85507236c8a8c672a92e1e9
baezbenjamin <baezbenjamin03@gmail.com>
2025-12-10 21:37:37 -0300

    Deleted the target blanck option

--- script/screenshot.js
+++ script/screenshot.js
@@ -2,22 +2,6 @@ const ssButton = document.querySelector("a");
 
 const siteAddress = window.location.href;
 
-const newURL = `https://shot.screenshotapi.net/v3/screenshot?token=RA18FXX-4804ZR6-NZRC1SC-MAP6P5R&fresh=true&url=${siteAddress}&output=image&file_type=png&wait_for_event=load`
-// console.log(siteAddress);
+const newURL = `https://shot.screenshotapi.net/v3/screenshot?token=KRDVN1X-P0HMYE3-J6PHZ2P-7H71FNQ&fresh=true&url=${siteAddress}&output=image&file_type=png&wait_for_event=load`;
 
-// ssButton.setAttribute("href", takeImage);
-
-async function setScreenshotButton() {
-    try {
-        const response = await fetch(newURL);
-        if (response.ok) {
-            ssButton.setAttribute("href", response);
-        } else {
-            throw Error(await response.text());
-        }
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-setScreenshotButton();
+ssButton.setAttribute("href", newURL);
